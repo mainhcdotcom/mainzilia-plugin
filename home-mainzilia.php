cd3347fc6400d2bffd3b53f2917bdaf2920b55e6
AsmaaHassan166 <asmaahassan166@gmail.com>
2021-09-29 15:15:15 +0200

    edit whatsapp link

--- home-mainzilia.php
+++ home-mainzilia.php
@@ -42,7 +42,7 @@
                         اول تطبيق يساعدك علي حجز استشارات مع امهر الأطباء بضغطة زر واحدة
                     </p>
                     <div class="button-container">
-                    <a href="https://wa.me/00966553564646/?text=%D8%B4%D9%83%D8%B1%D8%A7%20%D9%84%D8%AA%D9%88%D8%A7%D8%B5%D9%84%D9%83%D9%85%20%D9%85%D8%B9%20%D8%AE%D8%AF%D9%85%D8%A7%D8%AA%20%D9%85%D9%86%D8%B2%D9%84%D9%8A%D8%A7%20%D8%A7%D9%84%D8%B5%D8%AD%D9%8A%D8%A9" target="_blank"><button>أحجز زيارة الأن</button></a>
+                    <a href="https://wa.me/00966553564646" target="_blank"><button>أحجز زيارة الأن</button></a>
                     </div>
                 </div>
                 <div class="left-header col-md-5 ">
